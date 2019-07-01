269db669e452b694ee3085c46fb5c10f8ffd19cb
Joseph P <jpark041@gmail.com>
2019-07-01 20:06:06 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+  puts "hi #{name}"
+end
+
+def introduction_with_language(name, language)
+  puts "name is #{name}, and language is #{language}"
+end
+
+def introduction_with_language_optional(name, language)
+  language = "Ruby"
+end
