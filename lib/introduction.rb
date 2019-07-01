4bdcab55451b271cd42804c7b3c8e8ed98f3672f
Joseph P <jpark041@gmail.com>
2019-07-01 20:08:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,3 +9,5 @@ end
 def introduction_with_language_optional(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+introduction_with_language_optional(joe, "ruby")
+introduction_with_language_optional()
